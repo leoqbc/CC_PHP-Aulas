eec1812132566a2062f72e1efa49f9b9733c9d7b
Leonardo Tumadjian <tumadjian@gmail.com>
2022-06-23 18:41:26 -0300

    Add laravel e xhprof no projeto

---
+++ Profiling/README.md
@@ -0,0 +1,7 @@
+# Profiling no Laravel usando XHProf
+
+## Como ativar?
+```http://localhost:8081?profiling=1```
+
+## Link do Gerenciador
+```http://localhost:8081/xhprof/xhprof_html/index.php```
---
+++ Profiling/database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'nome' => $this->faker->name(),
+            'descricao' => $this->faker->sentence(),
+            'valor' => $this->faker->randomFloat(2, 20, 30)
+        ];
+    }
+}
